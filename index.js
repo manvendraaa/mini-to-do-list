c4501a623fc97539f99e38ce93f94b0c5740066e
manvendra <manvendra.rathore1607@gmail.com>
2021-06-06 00:50:39 +0530

    added css

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('assets'));
 app.set('view engine' ,'ejs');
 var tasks = ["eat", "sleep","repeat"];
 app.get('/',(req,res) => {
